0896b0734085d2e405c9759b645176c5a80db062
netudabbah <netudabbah@gmail.com>
2024-01-17 16:16:11 -0300

    Cambios varios

--- clientes.py
+++ clientes.py
@@ -1,22 +1,28 @@
+"""
+migrar main.py
+
+"""
 from tabulate import tabulate 
 import pandas as pd 
 import csv 
 import os.path
 
+if os.path.isfile("./clientes.csv"): #CORRECTO
+    df = pd.read_csv("clientes.csv") 
+
+else: # pero si no existe aún
+    with open("clientes.csv", "w", newline="") as file: # lo creamos con los headers
+        writer = csv.writer(file)
+        writer.writerow(["Cliente", "Razon social", "Cuenta corriente", "Ultima compra", "Observaciones"])
+df = pd.read_csv("clientes.csv")
+
 
 
 print("Bienvenido a empresa fantasma 1234")
 
-def main():
-    if os.path.isfile("./clientes.csv"): #CORRECTO
-        df = pd.read_csv("clientes.csv") 
-
-    else: # pero si no existe aún
-        with open("clientes.csv", "w", newline="") as file: # lo creamos con los headers
-            writer = csv.writer(file)
-            writer.writerow(["Cliente", "Razon social", "Cuenta corriente", "Ultima compra", "Observaciones"])
-        df = pd.read_csv("clientes.csv")
-
+def main(): # igual
+    print("\n\033[92mHOME\033[0m\n")
+    df = pd.read_csv("clientes.csv") 
     lista_entera, _ =  activador()
     while True:
         i = input(
@@ -24,11 +30,9 @@ def main():
 "2. Finalizar programa\n"
 "Indique número: ").strip()              
         if i == "1":
-            
             if not df.empty:
                 print("\nLista actual:\n", tabulate(lista_entera, headers="keys", tablefmt="fancy_grid"), sep="")
                 while True:
-                    
                     que_hacer = input("Desea modificar algo de la lista?\n1. Si\n2. No\nIndique número: ").strip()
                     if que_hacer == "1":
                         modificar_lista()
@@ -42,16 +46,19 @@ def main():
             
             else:
                 print("\nLa lista está vacía aún\n")
-                x = input("1. Para agregar un cliente \n2. Para salir\nIndique número: ").strip()
+                while True:
+                    x = input("1. Para agregar un cliente \n2. Para salir\nIndique número: ").strip()  
+                    if x == "1":
+                        agregar_cliente()
+                        break
+                    elif x == 2:
+                        print("Programa finalizado sin cambios.")
+                        break
+                    else:
+                        print("\n\033[91mERROR\033[0m: Indique una de las opciones\n")
+                        continue
+                    
                 
-                if x == "1":
-                    agregar_cliente()
-                    break
-                
-                else:
-                    print("Programa finalizado sin cambios.")
-                    break
-            
         elif i == "2":
             print("Programa finalizado con éxito. ")
         
@@ -60,16 +67,64 @@ def main():
             continue
         break
 
-def modificar_lista():
-    global df 
+def activador(): # igual
+    solo_clientes = []
+    lista_entera = []
+    with open("clientes.csv", encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for i in reader:
+            solo_clientes.append(i['Cliente']) # q pasa si tengo un julian de aca y otro de alla?
+            lista_entera.append(i) 
+    return lista_entera, solo_clientes
+
+def agregar_cliente(): # igual
+    lista_entera, solo_clientes = activador()
     while True:
-        s = input("""\n
+        cliente = input("Nombre del cliente: ").title().strip()
+        if cliente == "":
+            print("\n\033[91mERROR\033[0m: Ponga un nombre válido")
+            continue
+        if cliente in solo_clientes:
+            esta = input(f"""
+\n\033[91mATENCIÓN\033[0m
+{cliente} ya está en la lista.
+Para modificar el existente, oprima 1.
+Para ir al menú inicial, oprima 2
+Para finalizar, oprima cualquier otra tecla
+
+Número: """
+).strip()
+            if esta == "1":
+                modificar_cliente()
+                break
+            elif esta == "2":
+                main()
+                break
+            else:
+                print("Se canceló la operación.")
+                break
+        razon_social = input("Razon social: ").title().strip()
+        cuenta_corriente = 0
+        observaciones = input("Escriba observaciones si las hay, si no, enter: ")        
+        with open("clientes.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([cliente, razon_social, cuenta_corriente, "", observaciones])
+        lista_entera, _ = activador()
+        mostrar_lista(lista_entera)
+        print("✅ Operación exitosa, good evening")
+        break
+
+
+def modificar_lista(): # igual
+    global df 
+    print("\nMenu:")
+    while True:
+        s = input("""
 1. Agregar cliente
 2. Modificar existente
 3. Finalizar programa
 Indique número: """
     ).strip()
-
         if s == "1":
             agregar_cliente()
 
@@ -85,54 +140,18 @@ Indique número: """
         break
 
 
-def agregar_cliente(): #CORRECTO
-    lista_entera, solo_clientes = activador()
-    while True:
-        cliente = input("Nombre del cliente: ").capitalize().strip()
-        if cliente == "":
-            print("\n\033[91mERROR\033[0m: Ponga un nombre válido")
-            continue
-        if cliente in solo_clientes:
-            esta = input(f"""
-\n\033[91mATENCIÓN\033[0m
-{cliente} ya está en la lista.
-Para modificar el existente, oprima 1.
-Para salir, oprima cualquier otra tecla
-número: """
-).strip()
-            if esta == "1":
-                modificar_cliente()
-                break
-            else:
-                print("Se canceló la operación.")
-                break
-        razon_social = input("Razon social: ").capitalize().strip()
-        cuenta_corriente = 0
-        observaciones = input("Escriba observaciones si las hay, si no, enter: ")
-        
-        with open("clientes.csv", "a", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow([cliente, razon_social, cuenta_corriente, "", observaciones])
-        lista_entera, _ = activador()
-        mostrar_lista(lista_entera)
-        print("✅ Operación exitosa, good evening")
-        volver()
-        break
-
-def modificar_cliente():
+def modificar_cliente(): # igual
     _, solo_clientes =  activador()
-    # MODIFICAR EXISTENTE
-    if not df.empty: # Si no esta vacío
+    while True:
+        cliente = input("\nIndique el cliente que quiere modificar (escriba salir si no es necesario): ").strip().title()
+        if "salir" in cliente.lower():
+            print("Operación finalizada con \033[92méxito\033[0m")
+            break 
+        if cliente not in solo_clientes: # chequeo que sea un producto existente
+            print("\n\033[91mERROR\033[0m: Escriba un cliente de la lista")
+            continue
         while True:
-            cliente = input("\nIndique el cliente que quiere modificar (escriba salir si no es necesario): ").strip().capitalize()
-            if "salir" in cliente.lower():
-                print("Operación finalizada con \033[92méxito\033[0m")
-                break 
-            if cliente not in solo_clientes: # chequeo que sea un producto existente
-                print("\n\033[91mERROR\033[0m: Escriba un cliente de la lista")
-                continue
-            while True:
-                x = input(
+            x = input(
                 """
 1) Cambiar nombre cliente
 2) Cambiar razón social
@@ -141,52 +160,52 @@ def modificar_cliente():
 5) Finalizar programa
 \nIndique número: """
 ).strip()
-                if x == "1":
-                    cambiar_nombre(cliente)                  
-                    break
-                
-                elif x == "2":
-                    cambiar_r_social(cliente)
-                    break
+            if x == "1":
+                cambiar_nombre(cliente)                  
+                break
+            
+            elif x == "2":
+                cambiar_r_social(cliente)
+                break
 
-                elif x == "3":
-                    eliminar(cliente)            
-                    break
+            elif x == "3":
+                eliminar(cliente)            
+                break
 
-                elif x == "4": 
-                    editar_observacion(cliente)
-                    break
-                
-                elif x == "7":
-                    break
+            elif x == "4": 
+                editar_observacion(cliente)
+                break
+            
+            elif x == "5":
+                break
 
-                else:
-                    print("\n\033[91mERROR\033[0m: Elija una de las opciones. Solo numeros.")
-                    continue
-            break
+            else:
+                print("\n\033[91mERROR\033[0m: Elija una de las opciones. Solo numeros.")
+                continue
+        break
 
-    else:
-        print("\n\033[91mERROR\033[0m: No hay articulos en la lista aún")
 
-def cambiar_nombre(cliente):
+def cambiar_nombre(cliente): # igual
     global df 
+    print("\n\033[92mSección\033[0m: cambiar nombre del cliente\n")
     while True:
-        cliente_nuevo = input("Nuevo nombre: ").capitalize().strip()
+        cliente_nuevo = input("Nuevo nombre: ").title().strip()
         if cliente_nuevo == "":
             print("\n\033[91mERROR\033[0m: Escriba un nombre válido")
             continue
         break
-    df.loc[df["Cliente"] == cliente, "Articulo"] = cliente_nuevo
+    df.loc[df["Cliente"] == cliente, "Cliente"] = cliente_nuevo
     df.to_csv("clientes.csv", index=False)
     lista_entera, _ = activador() # para actualizar la lista y poder escribirla
     mostrar_lista(lista_entera) 
     print("✅ Operación exitosa. Que andes bien. ")
     volver()
 
-def cambiar_r_social(cliente):
+def cambiar_r_social(cliente): # igual
     global df
+    print("\n\033[92mSección\033[0m: editar razón social\n")
     while True:
-        r_social = input("Nueva razón social: ").capitalize().strip()
+        r_social = input("Nueva razón social: ").title().strip()
         if r_social == "":
             print("\n\033[91mERROR\033[0m: Nombre inválido, reintente")
             continue
@@ -198,10 +217,12 @@ def cambiar_r_social(cliente):
     print("✅ Operación exitosa. Que andes bien. ")
     volver()
 
-def eliminar(cliente):
+def eliminar(cliente): # igual
     global df
-    if input("Presione 1 para confirmar elección, tenga en cuenta que no hay vuelta atrás: ").strip() == "1":
-        df = df[df["Articulo"] != cliente]
+    print("\n\033[92mSección\033[0m: eliminar articulo\n")
+    i = input("Presione 1 para confirmar elección, tenga en cuenta que no hay vuelta atrás: ").strip()
+    if i == "1":
+        df = df[df["Cliente"] != cliente]
         df.to_csv("clientes.csv", index=False)    
         lista_entera, _ = activador() # para actualizar la lista y poder escribirla
         mostrar_lista(lista_entera) 
@@ -211,10 +232,14 @@ def eliminar(cliente):
     volver()
 
 
-def editar_observacion(cliente):
+def editar_observacion(cliente): # igual
     global df
+    print("\n\033[92mSección\033[0m: cambiar observación del cliente\n")
     obs = input("Nueva observación (enter para eliminar la actual): ")
-    df.loc[df["Cliente"] == cliente, "Observaciones"] = obs
+    if obs == "":
+        df.loc[df["Cliente"] == cliente, "Observaciones"] = None
+    else: 
+        df.loc[df["Cliente"] == cliente, "Observaciones"] = obs
     df.to_csv("clientes.csv", index=False)
     lista_entera, _ = activador() # para actualizar la lista y poder escribirla
     mostrar_lista(lista_entera) 
@@ -224,17 +249,6 @@ def editar_observacion(cliente):
 
 def mostrar_lista(lista_entera):
     print(tabulate(lista_entera, headers="keys", tablefmt="fancy_grid"))
-
-
-def activador(): # HECHO
-    solo_clientes = []
-    lista_entera = []
-    with open("clientes.csv", encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for i in reader:
-            solo_clientes.append(i['Cliente']) # q pasa si tengo un julian de aca y otro de alla?
-            lista_entera.append(i) 
-    return lista_entera, solo_clientes
 
 
 def volver():
--- main.py
+++ main.py
@@ -1,19 +1,36 @@
+"""
+PROBLEMA:
+    Si agregas producto, y modificas lo q sea, 
+    se borra todo el csv
+
+    Pero si lo agregas, cerras el programa,
+    y despues lo abris para modificar, todo bien
+
+No lo provoca la linea 30, como pensé
+Tampoco el activador
+"""
 from tabulate import tabulate 
 import pandas as pd 
-import csv 
+import csv
 import os.path
+
+if os.path.isfile("./productos.csv"): # Si existe el programa, con los headers incluidos
+    df = pd.read_csv("productos.csv") # Nada mas hay a hacerlo dataframe 
+
+else:  # modificar acá
+    with open("productos.csv", "w", newline="") as file: # lo creamos con los headers
+        writer = csv.writer(file)
+        writer.writerow(["Articulo", "Cantidad", "Costo", "Precio", "Observaciones"])
+    df = pd.read_csv("productos.csv")
 
 print("°°°Bienvenido a empresa fantasma 123.")
 
-def main():
-    if os.path.isfile("./productos.csv"): # Si existe el programa, con los headers incluidos
-        df = pd.read_csv("productos.csv") # Nada mas hay a hacerlo dataframe 
 
-    else: # pero si no existe aún
-        with open("productos.csv", "w", newline="") as file: # lo creamos con los headers
-            writer = csv.writer(file)
-            writer.writerow(["Articulo", "Cantidad", "Costo", "Precio", "Observaciones"])
-        df = pd.read_csv("productos.csv")
+def main():
+    print("\033[92mHOME\033[0m\n")
+    df = pd.read_csv("productos.csv") 
+    # la idea aca es actualizar el df en caso
+    # que despues de modificar/agregar mande a volver()
     lista_entera, _ =  activador()
     while True:
         i = input(
@@ -36,14 +53,18 @@ def main():
                         continue
             else:
                 print("\nLa lista está vacía aún\n")
-                x = input("1. Para agregar un producto \n2. Para salir\nIndique número: ").strip()
-                if x == "1":
-                    agregar_producto()
-                    break
-                else:
-                    print("Programa finalizado sin cambios.")
-                    break
-            
+                while True:
+                    x = input("1. Para agregar un articulo \n2. Para salir\nIndique número: ").strip()  
+                    if x == "1":
+                        agregar_producto()
+                        break
+                    elif x == 2:
+                        print("Programa finalizado sin cambios.")
+                        break
+                    else:
+                        print("\n\033[91mERROR\033[0m: Indique una de las opciones\n")
+                        continue
+      
         elif i == "2":
             print("Programa finalizado con éxito. ")
         
@@ -56,35 +77,40 @@ def activador():
     solo_articulos = []
     lista_entera = []
     with open("productos.csv", encoding='utf-8') as file:
-        reader = csv.DictReader(file)
+        reader = csv.DictReader(file) # modificar
         for i in reader:
-            # Para poder verificar q el articulo a modificar exista                 
             solo_articulos.append(i['Articulo'])
-            # Para mostrar el csv, lo hago lista
             lista_entera.append(i) 
     return lista_entera, solo_articulos
 
 def agregar_producto():
     lista_entera, solo_articulos = activador()
     while True:
-        nnombre = input("Nombre del articulo: ").capitalize().strip()
+        print("\nNUEVO PRODUCTO\n")
+        nnombre = input("Nombre del articulo: ").title().strip()
         if nnombre == "":
             print("\n\033[91mERROR\033[0m: Ponga un nombre válido")
             continue
         if nnombre in solo_articulos:
             esta = input(f"""
-\n\033[91mATENCIÓN\033[0m
+\033[91mATENCIÓN\033[0m
 {nnombre} ya está en la lista.
 Para modificar el existente, oprima 1.
-Para salir, oprima cualquier otra tecla
-número: """
+Para ir al menu inicial, oprima 2
+Para finalizar, cualquier otra tecla
+
+Número: """
 ).strip()
             if esta == "1":
                 modificar_producto()
                 break
+            elif esta == "2":
+                main() 
+                break  
             else:
                 print("Se canceló la operación.")
                 break
+                
         while True:
             try:
                 cantidad = int(input("Cantidad: "))
@@ -108,53 +134,51 @@ número: """
             break
         observaciones = input("Escriba observaciones si las hay, si no, enter: ")
         with open("productos.csv", "a", newline="") as file:
-            writer = csv.writer(file)
+            writer = csv.writer(file) # modificar
             writer.writerow([nnombre, cantidad, costo, precio_final, observaciones])
         lista_entera, solo_articulos = activador()
         mostrar_lista(lista_entera)
         print("✅ Operación exitosa, good evening")
-        volver()
         break
 
 
 def modificar_lista():
-    global df 
+    global df
+    print("\nMenu:") 
     while True:
-        s = input("""\n
+        s = input("""
 1. Agregar producto
 2. Modificar existente
 3. Finalizar programa
 Indique número: """
     ).strip()
-        if s not in ["1", "2", "3"]:
+
+        if s == "1":
+            agregar_producto()
+
+        elif s == "2":
+            modificar_producto()
+        elif s == "3":
+            print("✅ Operación finalizada con éxito") 
+        else:
             print("\n\033[91mERROR\033[0m: Indique una opción válida.")
             continue
         break
-
-    if s == "1":
-        # AGREGA PRODUCTO NUEVO
-        agregar_producto()
-
-    elif s == "2":
-        modificar_producto()
-    elif s == "3":
-            print("✅ Operación finalizada con éxito")    
+   
         
 
 def modificar_producto():
     _ , solo_articulos =  activador()
-    # MODIFICAR EXISTENTE
-    if not df.empty: # Si no esta vacío
+    while True:
+        articulo = input("\nIndique el artículo que quiere modificar (escriba salir si no es necesario): ").strip().capitalize()
+        if "salir" in articulo.lower():
+            print("Operación finalizada con \033[92méxito\033[0m")
+            break 
+        if articulo not in solo_articulos: # chequeo que sea un producto existente
+            print("\n\033[91mERROR\033[0m: Escriba un producto de la lista")
+            continue
         while True:
-            articulo = input("\nIndique el artículo que quiere modificar (escriba salir si no es necesario): ").strip().capitalize()
-            if "salir" in articulo.lower():
-                print("Operación finalizada con \033[92méxito\033[0m")
-                break 
-            if articulo not in solo_articulos: # chequeo que sea un producto existente
-                print("\n\033[91mERROR\033[0m: Escriba un producto de la lista")
-                continue
-            while True:
-                x = input(
+            x = input(
                 """
 1) Cambiar nombre articulo
 2) Cambiar cantidad
@@ -165,50 +189,48 @@ def modificar_producto():
 7) Finalizar programa
 \nIndique número: """
 ).strip()
-                if x == "1":
-                    cambiar_nombre(articulo)
-                    break
-                
-                elif x == "2":
-                    cambiar_cantidad(articulo)
-                    break
+            if x == "1":
+                cambiar_nombre(articulo)
+                break
+            
+            elif x == "2":
+                cambiar_cantidad(articulo)
+                break
 
-                elif x == "3":
-                    cambiar_costo(articulo)
-                    break
-                
-                elif x == "4":
-                    editar_precio(articulo)
-                    break                
+            elif x == "3":
+                cambiar_costo(articulo)
+                break
+            
+            elif x == "4":
+                editar_precio(articulo)
+                break                
 
-                elif x == "5":
-                    eliminar(articulo)            
-                    break
-                elif x == "6": 
-                    editar_observacion(articulo)
-                    break
-                elif x == "7":
-                    break
+            elif x == "5":
+                eliminar(articulo)            
+                break
+            elif x == "6": 
+                editar_observacion(articulo)
+                break
+            elif x == "7":
+                break
 
-                else:
-                    print("\n\033[91mERROR\033[0m: Elija una de las opciones. Solo numeros.")
-                    continue
-            break
-
-    else:
-        print("\n\033[91mERROR\033[0m: No hay articulos en la lista aún")
+            else:
+                print("\n\033[91mERROR\033[0m: Elija una de las opciones. Solo numeros.")
+                continue
+        break
 
 def cambiar_nombre(articulo):
     global df 
+    print("\n\033[92mSección\033[0m: cambiar nombre del articulo\n")
     while True:
-        articulo_nuevo = input("Nuevo nombre: ").capitalize().strip()
+        articulo_nuevo = input("Nuevo nombre: ").title().strip()
         if articulo_nuevo == "":
             print("\n\033[91mERROR\033[0m: Escriba un nombre válido")
             continue
         break
     df.loc[df["Articulo"] == articulo, "Articulo"] = articulo_nuevo
     df.to_csv("productos.csv", index=False)
-    lista_entera, solo_articulos = activador() # para actualizar la lista y poder escribirla
+    lista_entera, _ = activador() # para actualizar la lista y poder escribirla
     mostrar_lista(lista_entera) 
     print("✅ Operación exitosa. Que andes bien. ")
     volver()
@@ -216,6 +238,7 @@ def cambiar_nombre(articulo):
 
 def cambiar_cantidad(articulo):
     global df
+    print(f"\n\033[92mSección\033[0m: cambiar cantidad del articulo: {articulo}\n")
     while True:
         try:
             n_cant = int(input("Cantidad nueva: "))
@@ -233,9 +256,10 @@ def cambiar_cantidad(articulo):
 
 def cambiar_costo(articulo):
     global df 
+    print("\n\033[92mSección\033[0m: cambiar costo del articulo\n")
     while True:
         try:
-            n_costo = int(input("Costo nuevo: "))
+            n_costo = float(input("Costo nuevo: "))
         except ValueError:
             print("\n\033[91mERROR\033[0m: Ingrese un numero válido")
             continue
@@ -250,6 +274,7 @@ def cambiar_costo(articulo):
 
 def editar_precio(articulo):
     global df
+    print("\033[92mSección\033[0m: cambiar precio del artículo")
     while True:
         try:
             precio_n = float(input("Nuevo precio: "))
@@ -257,7 +282,7 @@ def editar_precio(articulo):
             print("\n\033[91mERROR\033[0m: Solo números y puntos admitidos.")
             continue
         break
-    df.loc[df["Articulo"] == articulo, "Precio final"] = precio_n
+    df.loc[df["Articulo"] == articulo, "Precio"] = precio_n
     df.to_csv("productos.csv", index=False)
     lista_entera, _ = activador() # para actualizar la lista y poder escribirla
     mostrar_lista(lista_entera) 
@@ -266,8 +291,12 @@ def editar_precio(articulo):
 
 def editar_observacion(articulo):
     global df
+    print("\n\033[92mSección\033[0m: cambiar observación del articulo\n")
     obs = input("Nueva observación (enter para eliminar la actual): ")
-    df.loc[df["Articulo"] == articulo, "Observaciones"] = obs
+    if obs == "":
+        df.loc[df["Articulo"] == articulo, "Observaciones"] = None
+    else: 
+        df.loc[df["Articulo"] == articulo, "Observaciones"] = obs
     df.to_csv("productos.csv", index=False)
     lista_entera, _ = activador() # para actualizar la lista y poder escribirla
     mostrar_lista(lista_entera) 
@@ -277,6 +306,7 @@ def editar_observacion(articulo):
 def eliminar(articulo): # EDITAR EN CASO Q HAYAN DOS CON MISMO NOMBRE 
                         # (POR EJ: DOS COCA PERO UNO EN OBS DICE 1 LT Y EL OTRO DICE LATA)
     global df
+    print("\n\033[92mSección\033[0m: eliminar articulo\n")
     if input("Presione 1 para confirmar elección, tenga en cuenta que no hay vuelta atrás: ").strip() == "1":
         df = df[df["Articulo"] != articulo]
         df.to_csv("productos.csv", index=False)
